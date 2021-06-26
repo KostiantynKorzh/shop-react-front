995beb6f13844fe2652ab9a20b48e6d4e9be87ad
Kostiantyn Korzh <kostiantyn_korzh@epam.com>
2021-06-26 13:48:32 +0300

    Add media to card for ItemCard

--- src/components/catalogue/ItemCard.js
+++ src/components/catalogue/ItemCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Card, CardContent, CardHeader} from "@material-ui/core";
+import {Card, CardContent, CardHeader, CardMedia} from "@material-ui/core";
 
 const ItemCard = (props) => {
     return (
         <>
             <Card style={{textAlign: 'center'}}>
                 <CardHeader title={props.title}/>
+                <CardMedia
+                    component="img"
+                    height="150"
+                    image={"https://img.fozzyshop.com.ua/197899-large_default/batonchik-snickers-s-arakhisom-v-molochnom-shokolade.jpg"}
+                />
                 <CardContent>{props.price}</CardContent>
             </Card>
         </>
