61aeb2437ea68ba741b6a3e2d645854f8426857a
Kostiantyn Korzh <kostiantyn_korzh@epam.com>
2021-08-04 10:41:06 +0300

    Test commit for Jenkins

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
                     <Link to={"/profile"}>Profile</Link>
                 </div>
                 <div>
-                    <Link to={"/login"}>Login</Link>
+                    <Link to={"/login"}>!Login!</Link>
                 </div>
                 <div>
                     <Link to={"/signup"}>Signup</Link>
